e375793b5891f61adca06a25cc3a8201decfa86f
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-08-08 16:35:08 +0800

    some other test spec

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 const myFunc = (a: number): number => {
     return a * a;
 }
-export default myFunc;
+
+let arr1 = ['cat', 'dog', 'horse'];
+let arr2 = ['cat', 'dog', 'horse'];
+
+console.log(arr1);
+export { arr1, arr2, myFunc as default };
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,10 @@
 import myFunc from '../index';
+import {arr1, arr2} from '../index';
 
 it('expect myFunc(5) to equal 25', () => {
     expect(myFunc(5)).toEqual(25);
+});
+
+it("expects arr1 to refer to arr2", () => {
+    expect(arr1).toEqual(arr2);
 });
