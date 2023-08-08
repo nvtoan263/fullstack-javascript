df6b1d83d5705967be61db9ed83b6cb80f5ff934
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-08-08 16:46:05 +0800

    Add src excercise 9

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-const myFunc = (a: number): number => {
-    return a * a;
-}
+import arrays from './utilities/arrays.js';
+import numbers from './utilities/numbers.js';
+import strings from './utilities/strings.js';
 
-let arr1 = ['cat', 'dog', 'horse'];
-let arr2 = ['cat', 'dog', 'horse'];
+const numArr = [3, 4, 5, 6];
+const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+const arrSum = arrays.addArr(numArr);
+const mixArr = arrays.concatArr(numArr, wordArr);
+const myNum = ('15' as unknown) as number % 2;
+const five = parseInt('5');
 
-console.log(arr1);
-export { arr1, arr2, myFunc as default };
+
+console.log(arrays.cut3(mixArr));
+console.log(numbers.sum(arrSum, myNum));
+console.log(strings.capitalize('the quick brown fox'));
+console.log(numbers.multiply(five, 8));
+console.log(arrays.lgNum(mixArr));
