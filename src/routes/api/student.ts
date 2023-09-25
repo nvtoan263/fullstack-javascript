7c3d1e9b3ee0d7a0b0559ed0c54e3ba21abd25c0
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-09-25 15:37:57 +0800

    Remove other project files and fix dependencies issue

--- src/routes/api/student.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';   
-const student = express.Router();
-
-student.get('/', (req, res) => { 
-    res.send("This is route for /api/student");
-});
-
-export default student;
--- src/routes/api/teacher.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';   
-const teacher = express.Router();
-
-teacher.get('/', (req, res) => { 
-    res.send("This is route for /api/teacher");
-});
-
-export default teacher;
